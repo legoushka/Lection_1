c4254ed025d27717b0f9d6c4ab7e4d5b119ded36
legoushka <96149010+legoushka@users.noreply.github.com>
2022-02-20 19:13:32 +0300

    feat: Intent call permission request

--- app/src/main/java/com/example/lection_1/MainActivity.kt
+++ app/src/main/java/com/example/lection_1/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.lection_1
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val CALL_PERMISSION_CODE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,9 +25,22 @@ class MainActivity : AppCompatActivity() {
         }
 
         findViewById<Button>(R.id.button2).setOnClickListener {
-            val phoneNumber = Uri.parse("tel:88123263163")
-            val callIntent = Intent(Intent.ACTION_DIAL, phoneNumber)
-            startActivity(callIntent)
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.CALL_PHONE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.CALL_PHONE),
+                    CALL_PERMISSION_CODE
+                )
+
+            } else {
+                val phoneNumber = Uri.parse("tel:89642905334")
+                val callIntent = Intent(Intent.ACTION_CALL, phoneNumber)
+                startActivity(callIntent)
+            }
         }
     }
 }
